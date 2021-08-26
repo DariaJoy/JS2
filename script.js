14e4e41ae578d841debed2379dfcd1d1cccd44a8
Daria Dzhagaryan <darya_joy@mail.ru>
2021-08-26 18:26:55 +0300

    Задание 2

--- script.js
+++ script.js
@@ -1,22 +1,89 @@
-const goods = [
-    { title: 'Pencil & Paper Co. Take Note Pens, Set of 4', price: '$11.20', img: 'img/Note_Pens_Set_of_4.jpg' },
-    { title: 'Golden Acrylic Scissors', price: '$22.00', img: 'img/Golden_Acrylic_Scissors.jpg' },
-    { title: 'Acrylic Desk Organizer', price: '$32.00', img: 'img/Acrylic_Desk_Organizer.jpg' },
-    { title: 'Sprout Plantable Colored Pencils, Set of 5', price: '$16.00', img: 'img/Sprout_Plantable_Colored_Pencils_Set_of_5.jpg' },
-    { title: 'Giant Paper Clips, Set of 3', price: '$10.00', img: 'img/Giant_Paper_Clips_Set_of_3.jpg' },
-    { title: 'Leather Tech Pouch', price: '$58.00', img: 'img/Leather_Tech_Pouch.jpg' },
-];
-
-const renderGoodsItem = (title = 'Title', price = 0, img = '') => {
-    return `<div class="goodsItem"><a class="itemPage" href="#"><h3 class="catalogH3">${title}</h3><img src="${img}" class="catalogImg"><p class="catalogP">${price}</p></a><button class="itemBtn">Add to Basket</button></div>`;
+class GoodsItem {
+    constructor(title, price, img) {
+        this.title = title;
+        this.price = price;
+        this.img = img;
+    }
+    render() {
+        return `<div class="goodsItem">
+        <a class="itemPage" href="#">
+        <h3 class="catalogH3">${this.title}</h3>
+        <img src="${this.img}" class="catalogImg">
+        <p class="catalogP">${this.price}</p></a>
+        <button class="itemBtn" onclick="findQuantity()">Add to Basket</button>
+           </div>`
+    }
 };
 
-const renderGoodsList = (list = []) => {
-    let html = ''
-    list.map(item => html += renderGoodsItem(item.title, item.price, item.img));
-    document.querySelector('.goodsList').innerHTML = html
-}
+class GoodsList {
+    constructor() {
+        this.goods = [];
+    }
+    fetchGoods() {
+        this.goods = [
+            { title: 'Pencil & Paper Co. Take Note Pens, Set of 4', price: '$11.20', img: 'img/Note_Pens_Set_of_4.jpg' },
+            { title: 'Golden Acrylic Scissors', price: '$22.00', img: 'img/Golden_Acrylic_Scissors.jpg' },
+            { title: 'Acrylic Desk Organizer', price: '$32.00', img: 'img/Acrylic_Desk_Organizer.jpg' },
+            { title: 'Sprout Plantable Colored Pencils, Set of 5', price: '$16.00', img: 'img/Sprout_Plantable_Colored_Pencils_Set_of_5.jpg' },
+            { title: 'Giant Paper Clips, Set of 3', price: '$10.00', img: 'img/Giant_Paper_Clips_Set_of_3.jpg' },
+            { title: 'Leather Tech Pouch', price: '$58.00', img: 'img/Leather_Tech_Pouch.jpg' },
+        ];
+    }
+    render() {
+        let listHtml = '';
+        this.goods.forEach(good => {
+            const goodItem = new GoodsItem(good.title, good.price, good.img);
+            listHtml += goodItem.render();
+        });
+        document.querySelector('.goodsList').innerHTML = listHtml;
+    }
+};
+const list = new GoodsList();
+list.fetchGoods();
+list.render();
 
-renderGoodsList(goods);
+class CartGoodsList extends GoodsList {
+    constructor(quantity) {
+        super();
+        this.quantity = quantity;
+        findQuantity();
+    }
+    findQuantity() {
+        // Метод добавления одного товара в корзину при нажатии кнопки на странице index
+        //Можно ли отрисвать новую карточку товара в корзине?
+    }
+};
+
+
+
+
+
+
+// const goods = [
+//     { title: 'Pencil & Paper Co. Take Note Pens, Set of 4', price: '$11.20', img: 'img/Note_Pens_Set_of_4.jpg' },
+//     { title: 'Golden Acrylic Scissors', price: '$22.00', img: 'img/Golden_Acrylic_Scissors.jpg' },
+//     { title: 'Acrylic Desk Organizer', price: '$32.00', img: 'img/Acrylic_Desk_Organizer.jpg' },
+//     { title: 'Sprout Plantable Colored Pencils, Set of 5', price: '$16.00', img: 'img/Sprout_Plantable_Colored_Pencils_Set_of_5.jpg' },
+//     { title: 'Giant Paper Clips, Set of 3', price: '$10.00', img: 'img/Giant_Paper_Clips_Set_of_3.jpg' },
+//     { title: 'Leather Tech Pouch', price: '$58.00', img: 'img/Leather_Tech_Pouch.jpg' },
+// ];
+
+// const renderGoodsItem = (title = 'Title', price = 0, img = '') => {
+//     return `<div class="goodsItem">
+//     <a class="itemPage" href="#">
+//     <h3 class="catalogH3">${title}</h3>
+//     <img src="${img}" class="catalogImg">
+//     <p class="catalogP">${price}</p></a>
+//     <button class="itemBtn">Add to Basket</button>
+//     </div>`;
+// };
+
+// const renderGoodsList = (list = []) => {
+//     let html = ''
+//     list.map(item => html += renderGoodsItem(item.title, item.price, item.img));
+//     document.querySelector('.goodsList').innerHTML = html
+// }
+
+// renderGoodsList(goods);
 
 
